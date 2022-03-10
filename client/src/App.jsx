e587ec0495688a1edd6d92c2181f515bbf793732
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-10 15:41:19 +0200

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Home from './pages/Home'
-import Mall from './pages/Mall'
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import Mall from "./pages/Mall";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/mall/:id" element={<Mall />} />
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:location/:dates/:guests" element={<Home />} /> {/*filter search*/ }
+        <Route path="/malls" element={<Mall />} />{/*all malls*/ }
+        <Route path="/mall/:id" element={<Mall />} />{/*single mall*/ }
+        <Route path="/mall/:id/room/:roomId" element={<Mall />} />{/*room in single mall*/ }
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
