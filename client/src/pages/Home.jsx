52cff9b53e9a547235c29c111fe29d21f4680922
Rotem <rotem.barsela95@gmail.com>
2022-03-10 05:14:45 +0200

    mallslist

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Body from '../components/Body'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Navbar />
             <Header />
+            <Body />
             <Footer />
         </div>
     )
