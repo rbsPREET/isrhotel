142ef068f6433f23ca17303dbbcdf0c23e3f681d
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-11 02:56:59 +0200

    .

--- client/src/components/MallsListSection.jsx
+++ client/src/components/MallsListSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import classes from "../css/MallsList.module.css";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+// import classes from "../css/MallsList.module.css";
+// import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+// import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import Section from "../ui/Section";
 import Carousel from "../ui/Carousel"
 
--- client/src/ui/Carousel.jsx
+++ client/src/ui/Carousel.jsx
@@ -1,30 +1,29 @@
-import { Component } from "react";
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { mallsData } from "../mockData";
 import Mall from "../components/Mall";
 
-export default class CenterMode extends Component {
-    render() {
-        const settings = {
-            className: "center",
-            centerMode: true,
-            infinite: true,
-            centerPadding: "60px",
-            slidesToShow: 3,
-            speed: 500,
-        };
-        return (
-            <div>
-                <Slider {...settings}>
-                    <div>
-                        {mallsData.map((item) => (
-                            <Mall key={item} item={item} />
-                        ))}
-                    </div>
-                </Slider>
-            </div>
-        );
-    }
-}
+const Carousel = () => {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          {mallsData.map((item) => (
+            <Mall key={item} item={item} />
+          ))}
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
