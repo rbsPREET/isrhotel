9489f5e6b5bdc2052363e395b231008c6c2b085f
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-11 04:48:16 +0200

    .

--- client/src/components/MallPage/Header.jsx
+++ client/src/components/MallPage/Header.jsx
@@ -1,19 +1,9 @@
-import React from 'react'
-import Carousel from "../../ui/Carousel"
-import classes from "../../css/MallPage/Header.module.css";
-import Section from '../../ui/Section';
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import { mallsData } from '../../mockData'
-import Mall from '../Mall'
-import Carousel from '../../ui/Carousel';
+import React from "react";
+import Carousel from "../../ui/Carousel";
+
 
 const Header = () => {
-    return (
-        <div className={classes.container}>
-            <Carousel/>
-        </div>
-    )
-}
+  return <Carousel />;
+};
 
-export default Header
+export default Header;
