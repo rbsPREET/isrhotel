41b46552b1961695d5e9d49f7de38f3f31ce4fac
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-16 23:46:37 +0200

    fixing-stars-appearance
    fixing problem cause by mall.reviews.stars => need to calculate stars

--- client/src/components/malls/Body.jsx
+++ client/src/components/malls/Body.jsx
@@ -13,119 +13,132 @@ import { GetMall, UpdateMallReviewsStars } from "../../store/mall";
 import { useLocation } from "react-router-dom";
 
 const Body = () => {
-    const location = useLocation(); // get the mall Id from the path
-    const state = useSelector((state) => state.mall.information);
-    const dispatch = useDispatch();
-    const mallId = location.pathname.split("/")[2];
+  const location = useLocation(); // get the mall Id from the path
+  const state = useSelector((state) => state.mall.information);
+  const dispatch = useDispatch();
+  const mallId = location.pathname.split("/")[2];
 
-    // Redux Funcs Calls
-    const handleStarsChange = (starValue) => {
-        dispatch(
-            UpdateMallReviewsStars({
-                ...state.reviews, starValue, mallId
-            })
-        )
-    }
+  // Redux Funcs Calls
+  const handleStarsChange = (starValue) => {
+    dispatch(
+      UpdateMallReviewsStars({
+        ...state.reviews,
+        starValue,
+        mallId,
+      })
+    );
+  };
 
-    useEffect(() => {
-        dispatch(GetMall(mallId));
-    }, [dispatch, mallId]);
+  useEffect(() => {
+    dispatch(GetMall(mallId));
+  }, [dispatch, mallId]);
 
-    const tabSections = [
-        {
-            title: "Overview",
-            data: state.description,
-        },
-        {
-            title: "Pricing",
-            data: "test2",
-        },
-        {
-            title: "Location",
-            data: "test3",
-        },
-    ];
+  const tabSections = [
+    {
+      title: "Overview",
+      data: state.description,
+    },
+    {
+      title: "Pricing",
+      data: "test2",
+    },
+    {
+      title: "Location",
+      data: "test3",
+    },
+  ];
 
-    const bookingTimes = [
-        {
-            value: "9:00 am - 9:30 am",
-        },
-        {
-            value: "10:00 am - 10:30 am",
-        },
-        {
-            value: "11:00 am - 11:30 am",
-        },
-        {
-            value: "13:00 pm - 13:30 pm",
-        },
-    ];
+  const bookingTimes = [
+    {
+      value: "9:00 am - 9:30 am",
+    },
+    {
+      value: "10:00 am - 10:30 am",
+    },
+    {
+      value: "11:00 am - 11:30 am",
+    },
+    {
+      value: "13:00 pm - 13:30 pm",
+    },
+  ];
 
-    let starsArr = []
-    for (let index = 1; index <= 5; index++) {
-        if (state.reviews.stars.four.value >= index)
-            starsArr.push({
-                value: index,
-                color: "gold"
-            })
-        else
-            starsArr.push({
-                value: index,
-                color: "gray"
-            })
-    }
+  let starsArr = [];
+  let countReviews = 0;
+  const reduceReviews = Object.entries(state.reviews.stars).reduce(
+    (start, num) => (countReviews = countReviews + +num[1].count)
+  );
 
-    return (
-        <Section className={classes.container}>
-            <div className={classes.left}>
-                {/* Left side => Top */}
-                <div className={classes.top}>
-                    <h1 className={classes.mainTitle}>{state.title} Hotel</h1>
-                    <FlexRow centerColumn>
-                        <LocationIcon style={{ marginLeft: "-5px" }} />
-                        &nbsp;
-                        <p className={classes.addressTitle}>{state.address}</p>
-                    </FlexRow>
-                    <FlexRow centerColumn>
-                        <p className={classes.stars}>
-                            {Object.entries.starsArr.map((star) =>
-                                <StarIcon onClick={() => handleStarsChange(star.value)} key={star.value} htmlColor={star.color} />
-                            )}
-                        </p>
-                        &nbsp;
-                        <span className={classes.addressTitle}>({state.reviews.stars} reviews)</span>
-                    </FlexRow>
-                    <div>
-                        <FlexRow>
-                            <LabTabs tabSections={tabSections} />
-                        </FlexRow>
-                    </div>
-                </div>
-                {/* Left side => Amenities */}
-                <div className={classes.mid}>
-                    <h1 className={classes.title}>Amenities</h1>
-                    <FlexRow wrap customWidth={70}>
-                        {Object.entries(state.amenities)?.map((item) => (
-                            <FlexRow key={item[0]} centerColumn customWidth={33}>
-                                {item[1].available ? (
-                                    <CheckBoxIcon htmlColor="green" />
-                                ) : (
-                                    <IndeterminateCheckBoxIcon htmlColor="red" />
-                                )}
-                                {item[1].text}
-                            </FlexRow>
-                        ))}
-                    </FlexRow>
-                </div>
-            </div>
-            {/* Right side */}
-            <div className={classes.right}>
-                <div className={classes.card}>
-                    <SideBar hotelName={state.title} bookingTimes={bookingTimes} />
-                </div>
-            </div>
-        </Section>
-    )
-}
+  for (let index = 1; index <= 5; index++) {
+    console.log(state.reviews.stars.four.value);
+    if (state.reviews.stars.four.value >= index)
+      starsArr.push({
+        value: index,
+        color: "gold",
+      });
+    else
+      starsArr.push({
+        value: index,
+        color: "gray",
+      });
+  }
+  return (
+    <Section className={classes.container}>
+      <div className={classes.left}>
+        {/* Left side => Top */}
+        <div className={classes.top}>
+          <h1 className={classes.mainTitle}>{state.title} Hotel</h1>
+          <FlexRow centerColumn>
+            <LocationIcon style={{ marginLeft: "-5px" }} />
+            &nbsp;
+            <p className={classes.addressTitle}>{state.address}</p>
+          </FlexRow>
+          <FlexRow centerColumn>
+            <p className={classes.stars}>
+              {starsArr.map((star) => (
+                <StarIcon
+                  onClick={() => handleStarsChange(star.value)}
+                  key={star.value}
+                  htmlColor={star.color}
+                />
+              ))}
+            </p>
+            &nbsp;
+            <span className={classes.addressTitle}>
+              ({reduceReviews} reviews)
+            </span>
+          </FlexRow>
+          <div>
+            <FlexRow>
+              <LabTabs tabSections={tabSections} />
+            </FlexRow>
+          </div>
+        </div>
+        {/* Left side => Amenities */}
+        <div className={classes.mid}>
+          <h1 className={classes.title}>Amenities</h1>
+          <FlexRow wrap customWidth={70}>
+            {Object.entries(state.amenities)?.map((item) => (
+              <FlexRow key={item[0]} centerColumn customWidth={33}>
+                {item[1].available ? (
+                  <CheckBoxIcon htmlColor="green" />
+                ) : (
+                  <IndeterminateCheckBoxIcon htmlColor="red" />
+                )}
+                {item[1].text}
+              </FlexRow>
+            ))}
+          </FlexRow>
+        </div>
+      </div>
+      {/* Right side */}
+      <div className={classes.right}>
+        <div className={classes.card}>
+          <SideBar hotelName={state.title} bookingTimes={bookingTimes} />
+        </div>
+      </div>
+    </Section>
+  );
+};
 
 export default Body;
