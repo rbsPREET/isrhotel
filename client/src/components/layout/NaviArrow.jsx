3d57117f0bce0f7c378dd0e4c6ee1aa2a16c8894
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-17 01:29:11 +0200

    .

--- client/src/components/layout/NaviArrow.jsx
+++ client/src/components/layout/NaviArrow.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 import classes from "../../css/layout/NaviArrow.module.css";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const NaviArrow = () => {
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-            /* you can also use 'auto' behaviour
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
                in place of 'smooth' */
-        });
-    };
+    });
+  };
 
+  return (
+    <div className={classes.container} onClick={scrollToTop}>
+      <KeyboardArrowUpIcon />
+    </div>
+  );
+};
 
-    return (
-        <div className={classes.container}>
-            <KeyboardArrowUpIcon onClick={scrollToTop} />
-        </div>
-
-    )
-}
-
-export default NaviArrow
+export default NaviArrow;
