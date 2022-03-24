4ee9587e21fb40775129183638306553ac7cd202
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-25 00:00:51 +0200

    .

--- client/src/components/home/GuestModal.jsx
+++ client/src/components/home/GuestModal.jsx
@@ -7,38 +7,36 @@ import RemoveIcon from "@mui/icons-material/Remove";
 const GuestModal = (props) => {
   return (
     <div className={classes.wrapper}>
-      <FlexRow justifyBetween>
+      <FlexRow className={classes.row} justifyBetween>
         <FlexColumn>
           <h4>Adults</h4>
           <p>Ages 13 or above</p>
         </FlexColumn>
         <FlexRow centerColumn>
           <AddIcon className={classes.icon} />
-          <h3 className={classes.amount}>0</h3>
+          <p className={classes.amount}>0</p>
           <RemoveIcon className={classes.icon} />
         </FlexRow>
       </FlexRow>
-      <FlexRow justifyBetween>
+      <FlexRow className={classes.row} justifyBetween>
         <FlexColumn>
           <h4>Children</h4>
-          <p>Ages 2–12
-
-</p>
+          <p>Ages 2–12</p>
         </FlexColumn>
         <FlexRow centerColumn>
           <AddIcon className={classes.icon} />
-          <h3 className={classes.amount}>0</h3>
+          <p className={classes.amount}>0</p>
           <RemoveIcon className={classes.icon} />
         </FlexRow>
       </FlexRow>
-      <FlexRow justifyBetween>
+      <FlexRow className={classes.row} justifyBetween>
         <FlexColumn>
           <h4>Infants</h4>
           <p>Ages 0–2</p>
         </FlexColumn>
         <FlexRow centerColumn>
           <AddIcon className={classes.icon} />
-          <h3 className={classes.amount}>0</h3>
+          <p className={classes.amount}>0</p>
           <RemoveIcon className={classes.icon} />
         </FlexRow>
       </FlexRow>
