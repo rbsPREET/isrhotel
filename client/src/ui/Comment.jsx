e6a7f879ae7ef6313ee7fac24cd3a6c192525fc7
Rotem <rotem.barsela95@gmail.com>
2022-03-25 18:13:20 +0300

    update

--- client/src/ui/Comment.jsx
+++ client/src/ui/Comment.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
+import classes from "../css/ui/Comment.module.css";
+import FlexColumn from "./FlexColumn";
+import FlexRow from "./FlexRow";
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
 
 const Comment = ({ comment }) => {
     return (
-        <div style={{ marginBottom: "30px" }}>
-            <p>{comment.username}</p>
-            <p>{comment.userLogo}</p>
-            <p>{comment.createdAt}</p>
-            <p>{comment.description}</p>
-            {comment.img?.map((item) => {
-                return (
-                    <img src={item.img} alt="img" />
-                )
-            })}
-            <p>{comment.stars}</p>
-            <p>{comment.likes}</p>
-        </div>
+        <FlexColumn fullSize className={classes.container}>
+            <FlexRow justifyBetween>
+                <img src={comment.userLogo} alt="logo" className={classes.userLogo} />
+                <FlexColumn className={classes.userDetails}>
+                    <FlexColumn style={{ flex: 2 }}>
+                        <span className={classes.user_name}>{comment.username}</span>
+                        <span className={classes.user_createdAt}>{comment.createdAt}</span>
+                        <p className={classes.user_description}>{comment.description}</p>
+                    </FlexColumn>
+                    <div style={{ flex: 1 }}>
+                        {comment.img?.map((item) => {
+                            return (
+                                <img className={classes.img_item} key={comment._id} src={item.img} alt="img" />
+                            )
+                        })}
+                        <div className={classes.likes}>
+                            <ThumbUpOutlinedIcon className={classes.thumbup_icon} />
+                            <p style={{ paddingLeft: "5px" }}>
+                                {comment.likes}
+                            </p>
+                        </div>
+                    </div>
+                </FlexColumn>
+                <p className={classes.stars}>{comment.stars}</p>
+            </FlexRow>
+
+            <hr />
+        </FlexColumn>
     )
 }
 
