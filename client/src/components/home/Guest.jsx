5dd8b23541fdbc301c2f40755c643aa65bdee4bf
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2022-03-28 02:47:41 +0300

    0

--- client/src/components/home/Guest.jsx
+++ client/src/components/home/Guest.jsx
@@ -4,20 +4,24 @@ import FlexColumn from "../../ui/FlexColumn";
 import FlexRow from "../../ui/FlexRow";
 import { InputWrapper } from "../../ui/InputWrapper";
 import GuestModal from "./GuestModal";
+import classes from "../../css/home/Guest.module.css";
 
 const Guest = (props) => {
+  const [activeModal, setActiveModal] = useState(false);
+  const [guestAmount, setGuestsAmount] = useState(1);
+  const openModal = () => {
+    setActiveModal(!activeModal);
+  };
 
-    const [activeModal, setActiveModal] = useState(false);
-
-    const openModal = () => {
-      setActiveModal(true);
-    };
-
+  const guestValue = (amount) => {
+    setGuestsAmount(amount);
+  };
 
   const modal = (
     <Transition unmountOnExit in={activeModal} timeout={150}>
       {(state) => (
         <GuestModal
+          guestValue={guestValue}
           activeModal={state === "entered" && activeModal}
           setActiveModal={setActiveModal}
         />
@@ -25,11 +29,13 @@ const Guest = (props) => {
     </Transition>
   );
   return (
-    <InputWrapper icon={props.icon} mainDiv={props.mainDiv}>
-      <FlexColumn relative onClick={()=>setActiveModal(!activeModal)}>
+    <InputWrapper icon={props.icon} mainDiv={classes.mainDiv}>
       {modal}
-        <h4>1 Guest</h4>
-        <p>Click to add more</p>
+      <FlexColumn relative onClick={openModal}>
+        <h4>
+          {guestAmount} {guestAmount > 1 ? "Guests" : "Guest"}
+        </h4>
+        {guestAmount == 1 && <p>Click to add more</p>}
       </FlexColumn>
     </InputWrapper>
   );
--- client/src/components/home/GuestModal.jsx
+++ client/src/components/home/GuestModal.jsx
@@ -3,40 +3,77 @@ import FlexColumn from "../../ui/FlexColumn";
 import FlexRow from "../../ui/FlexRow";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
+import { useEffect, useState } from "react";
 
 const GuestModal = (props) => {
+  const [amount, setAmount] = useState({
+    adults: 1,
+    childrens: 0,
+    infants: 0,
+  });
+  const increaseHandler = (amountName) => {
+    let newAmount = {};
+    Object.entries(amount).filter((a) => {
+      if (a[0] === amountName) {
+        newAmount[`${amountName}`] = a[1] + 1;
+      }
+    });
+    setAmount((prev) => {
+      return {
+        ...prev,
+        ...newAmount,
+      };
+    });
+  };
+
+  useEffect(() => {
+    let newAmount = Object.values(amount).reduce((a, b) => +a + +b);
+    props.guestValue(newAmount);
+    return () => {
+      newAmount = 0;
+    };
+  }, [increaseHandler]);
   return (
     <div className={classes.wrapper}>
-      <FlexRow className={classes.row} justifyBetween>
+      <FlexRow justifyBetween>
         <FlexColumn>
           <h4>Adults</h4>
           <p>Ages 13 or above</p>
         </FlexColumn>
         <FlexRow centerColumn>
-          <AddIcon className={classes.icon} />
-          <p className={classes.amount}>0</p>
+          <AddIcon
+            onClick={increaseHandler.bind(null, "adults")}
+            className={classes.icon}
+          />
+          <p className={classes.amount}>{amount.adults}</p>
           <RemoveIcon className={classes.icon} />
         </FlexRow>
       </FlexRow>
-      <FlexRow className={classes.row} justifyBetween>
+      <FlexRow className={classes.mt24} justifyBetween>
         <FlexColumn>
           <h4>Children</h4>
           <p>Ages 2–12</p>
         </FlexColumn>
         <FlexRow centerColumn>
-          <AddIcon className={classes.icon} />
-          <p className={classes.amount}>0</p>
+          <AddIcon
+            onClick={increaseHandler.bind(null, "childrens")}
+            className={classes.icon}
+          />
+          <p className={classes.amount}>{amount.childrens}</p>
           <RemoveIcon className={classes.icon} />
         </FlexRow>
       </FlexRow>
-      <FlexRow className={classes.row} justifyBetween>
+      <FlexRow className={classes.mt24} justifyBetween>
         <FlexColumn>
           <h4>Infants</h4>
           <p>Ages 0–2</p>
         </FlexColumn>
         <FlexRow centerColumn>
-          <AddIcon className={classes.icon} />
-          <p className={classes.amount}>0</p>
+          <AddIcon
+            onClick={increaseHandler.bind(null, "infants")}
+            className={classes.icon}
+          />
+          <p className={classes.amount}>{amount.infants}</p>
           <RemoveIcon className={classes.icon} />
         </FlexRow>
       </FlexRow>
